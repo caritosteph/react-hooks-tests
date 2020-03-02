aef7f443482e861fd6724114515f4e1f74482999
Carolyn Ulfe <carolyn.ulfe@gmail.com>
2020-03-02 01:21:13 +0100

    feat: Clean hook code

---
+++ src/CustomCounter/useCount.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const useCounter = ({ initialCount = 0, step = 1 } = {}) => {
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => setCount(c => c + step);
+  const decrement = () => setCount(c => c - step);
+
+  return { count, increment, decrement };
+};
+
+export default useCounter;
